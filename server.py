b95519243a59041f094300cf8019d6c024aa5b45
igruntplay <gaspar.onesto@gmail.com>
2022-04-03 10:34:09 -0300

    style

--- server.py
+++ server.py
@@ -3,14 +3,16 @@ from getCreds import mailCredentials
 from main import main
 app = Flask(__name__)
 
+app.add_url_rule('/photos/<path:filename>', endpoint='photos', view_func=app.send_static_file)
 @app.route('/')
 def index():
   return render_template('index.html')
 
 @app.route('/my-link/')
 def my_link():
-  main()
-  return 'Click.'
+  status=main()
+  if status==True:
+    return 'Contraseña cambiada con exito'
 
 if __name__ == '__main__':
   app.run(debug=True)
